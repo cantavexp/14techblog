b67563ff2a60270edbfb77610e754502e90acd8f
cantavexp <gunnadar@gmail.com>
2023-07-25 15:01:17 -0500

    blog

---
+++ README.md
@@ -0,0 +1,55 @@
+# Tech Blog
+
+This app is a tech blog where you can post add and login commits
+
+
+## TABLE OF CONTENTS
+- [Usage](#usage)  
+
+- [How app works](#usage)
+
+- [Technologies](#usage)
+
+- [Creater Info](#usage)
+
+
+## Usage
+
+1. blog / post and reading commits
+
+
+## How app works /Functionality
+
+User logins in and can read other users commits. User can also 
+post there own commits.
+
+## Technologies
+
+html
+javascript
+css
+README
+MVC order
+
+## Creater Info
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -3,19 +3,31 @@ const express = require('express');
 // Import express-session
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+
+
 // Set up sessions
 const sess = {
   secret: 'Super secret secret',
+  cookie: {
+    maxAge: 360000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
@@ -26,11 +38,12 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+app.use(require('./controllers/'));
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  sequelize.sync({ force: false });
 });
